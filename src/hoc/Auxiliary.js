6cb777ad9ee204cb37de28ae3e3560607a47e6f3
mimbele <mrym.ndr@gmail.com>
2020-09-12 11:03:44 +0430

    setting up obvious stuff

---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+//this component is merely for containing other components,
+// so that we can return adjacent top level jsx elements
+const auxiliary = props => props.children;
+
+export default auxiliary;
