2b0d30e05ce45070920e825a7603ac77703e5ff9
mimbele <mrym.ndr@gmail.com>
2020-09-20 18:51:37 +0430

    restructuring folders and using shouldComponentUpdate for performance improvement

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './Modal.module.css'
 import Backdrop from './../Backdrop/Backdrop'
 import Aux from '../../../hoc/Auxiliary'
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.cancelOrder}/>
-        <div 
-            className={styles.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? 1 : 0
-            }}>
-            {props.children}
-        </div> 
-    </Aux>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    render(){
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.cancelOrder}/>
+                <div 
+                    className={styles.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? 1 : 0
+                    }}>
+                    {this.props.children}
+                </div> 
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
