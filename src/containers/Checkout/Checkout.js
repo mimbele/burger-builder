550e517da0c79c59e406b5569741fae75463b234
mimbele <mrym.ndr@gmail.com>
2020-10-21 15:02:25 +0330

    connecting Checkout component to redux store

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 import styles from './Checkout.module.css'
 import Burger from '../../components/Burger/Burger'
 import Button from '../../components/UI/Button/Button'
@@ -8,26 +9,7 @@ import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: null,
-        price: 0,
         continued: false
-    }
-
-    componentWillMount () {
-        //super();
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {}
-        let price = 0
-        for (let param of query.entries()){
-            // now each 'param' is an array in this form: ['salad', '1']
-            if (param[0] === 'price') {
-                price = +param[1]
-            }
-            else {
-                ingredients[param[0]] = +param[1]
-            }
-        }
-        this.setState({ingredients: ingredients, totalPrice: price})
     }
 
     checkoutCancelled = () => {
@@ -40,14 +22,13 @@ class Checkout extends Component {
     }
 
     render() {
-        console.log(this.props)
         return (
             <div className={styles.Checkout}>
                 <h2>Hope You Enjoy This Burger! ^_^ </h2>
                 <div style={{width: '100%', margin: 'auto'}}>
-                    <Burger ingredients={this.state.ingredients}/>
+                    <Burger ingredients={this.props.ingredients}/>
                 </div>
-                <strong><p>Total Price: {this.state.totalPrice.toFixed(2)}$</p></strong>
+                <strong><p>Total Price: {this.props.totalPrice.toFixed(2)}$</p></strong>
                 
                 <Button hide={this.state.continued} clicked={this.checkoutCancelled} btnType='Danger'>Cancel</Button>
                 <Button hide={this.state.continued} clicked={this.checkoutContinued} btnType='Success'>Continue</Button>
@@ -55,12 +36,17 @@ class Checkout extends Component {
                     path={this.props.match.path + '/contact-data'} 
                     render={(props) => 
                         (<ContactData 
-                            ingredients={this.state.ingredients} 
-                            totalPrice={this.state.totalPrice} 
+                            ingredients={this.props.ingredients} 
+                            totalPrice={this.props.totalPrice} 
                             {...props} />)} />
             </div>
         );
     }
 }
 
-export default Checkout;
+const mapStateToProps = state => ({
+    ingredients: state.ingredients,
+    totalPrice: state.totalPrice
+})
+
+export default connect(mapStateToProps)(Checkout);
