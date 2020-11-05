7e86c3576a17cc6dc07cdbd01fb46cdc31b7f35b
mimbele <mrym.ndr@gmail.com>
2020-11-05 15:16:22 +0330

    fixed the previeous bug

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-//import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import styles from './Profile.module.css'
 import Button from '../../components/UI/Button/Button'
 import { logOut } from '../../store/actions/auth'
 
 class Profile extends Component {
 
+    state = { logout: false}
+
     logout = () => {
+        this.setState({logout:true})
         this.props.logout()
-        this.props.history.push('/')
     }
 
     render() {
@@ -17,7 +19,8 @@ class Profile extends Component {
         return (
             <div className={styles.Profile}>
                 <h4>This is Your Profile</h4>
-                <Button clicked={this.logout()} btnType='Danger'>Logout</Button>
+                <Button clicked={this.logout} btnType='Danger'>Logout</Button>
+                {this.state.logout ? <Redirect to='/'/> : null}
             </div>
         )
     }
